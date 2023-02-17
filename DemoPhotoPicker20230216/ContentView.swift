bf07824373132a910468711e8c8aca2ada5a8e6a
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-16 22:44:45 -0500

    DemoPhotoPicker20230216

--- DemoPhotoPicker20230216/ContentView.swift
+++ DemoPhotoPicker20230216/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var selected: [PhotosPickerItem] = []
+    @State var data: Data?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                if let data = data, let uiImage = UIImage(data: data) {
+                    VStack {
+                        HStack {
+                            Text("Selected photo: ")
+                                .bold()
+                            Spacer()
+                        }
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .padding()
+                }
+                Spacer()
+                    .frame(height: 50)
+                PhotosPicker(selection: $selected, maxSelectionCount: 1, matching: .images) {
+                    VStack {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .frame(width: 60, height: 45, alignment: .center)
+                        Text("Select a photo")
+                    }
+                }
+                .onChange(of: selected) { newValue in
+                    guard let selectedItem = selected.first else {
+                        return
+                    }
+                    selectedItem.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            if let data = data {
+                                self.data = data
+                            } else {
+                                print("Found nil in data")
+                            }
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Photo Picker")
         }
-        .padding()
     }
 }
 
