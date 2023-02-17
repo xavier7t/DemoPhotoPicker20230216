312944698df7cf6745de71ade8993a31f8f2b048
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-16 22:43:33 -0500

    Initial Commit

---
+++ DemoPhotoPicker20230216/DemoPhotoPicker20230216App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPhotoPicker20230216App.swift
+//  DemoPhotoPicker20230216
+//
+//  Created by Xavier on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoPhotoPicker20230216App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
